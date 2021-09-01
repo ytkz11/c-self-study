fadf9d182688100f280e1fca9993360e4efcc605
abner11Ytkz <ytkz11@163.com>
2021-09-01 12:31:08 +0800

    day3

---
+++ constants.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+ 
+int main()
+{
+	int num = 5;
+	printf("%d\n", num);
+   num =  8;
+   	printf("%d", num);
+   return 0;
+}
---
+++ input_.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+ 
+int main()
+{
+	int num;
+	printf("Enter your goal: ");
+ scanf("%d", &num);
+   	printf("your goal is %d", num);
+   return 0;
+}

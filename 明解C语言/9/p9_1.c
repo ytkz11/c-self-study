43d61e9341ca64c5f86ef72915b1272557c01879
ytkz11 <ytkz11@163.com>
2021-10-14 11:44:46 +0800

    9

---
+++ 明解C语言/9/p9_1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/*
+
+*/
+
+int main(void){
+	char str[] = "ABC\0DEF";
+	printf("×Ö·û´®strÎª%s¡£\n",str);
+
+
+
+
+	return 0;}
---
+++ 明解C语言/9/p9_2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/*
+将str变为空字符串
+*/
+
+int main(void){
+	char str[] = "ABC";
+	int i;
+	i = sizeof(str);
+	
+	printf("字符串str为%s。\n",i);
+
+
+
+
+	return 0;}

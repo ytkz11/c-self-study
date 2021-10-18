8cee97bee1410ee1b1aecb90cb69df07523123df
ytkz11 <ytkz11@163.com>
2021-10-18 17:50:02 +0800

    1018

---
+++ 明解C语言/9/p9_3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/*
+将str变为空字符串
+*/
+int str_char(const char str[], int c){
+	int len = 0;
+	
+	while (str[len] !='c')
+		len++;
+	if len
+		
+}
+int main(void){
+	char str[100];
+	
+	i = sizeof(str);
+	
+	printf("字符串str为%s。\n",i);
+
+
+
+
+	return 0;}

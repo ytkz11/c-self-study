77bbc8fa7d70a49b510c52b1068541000e1a4c8e
ytkz11 <ytkz11@163.com>
2022-01-19 15:23:21 +0800

    20220119

--- 明解C语言/11 字符串和指针/11_2.c
+++ 明解C语言/11 字符串和指针/11_2.c
@@ -3,20 +3,17 @@
 ×Ö·û´®Êý×é
 */
 
+
+
 int main(void){
 	int i;
 	char a[][8] = {"l","a","p"};
 	char *p[] = {"Paul","xx","ee"};
 	
-	int k = sizeof(p);
 
-	printf("%s",p[3]);
-
-	//printf("%d\n",k);
-	for (i=0;i<3;i++)
+	for (i=0;i< sizeof(a)/sizeof(a[0]);i++)
 		printf("a[%d] = \"%s\"\n",i,a[i]);
-	for (i=0;i<3;i++)
+	for (i=0;i< sizeof(p)/sizeof(p[0]);i++)
 		printf("p[%d] = \"%s\"\n",i,p[i]);
-
-	return 0;}
-
+	return 0;
+}
